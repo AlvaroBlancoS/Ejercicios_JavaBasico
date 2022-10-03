b961dee03aa991f9a601667c4c9e8513e38ea2ba
Álvaro Blanco Sanginés <35927676+AlvaroBlancoS@users.noreply.github.com>
2022-10-03 10:55:07 +0200

    Ejercicio Vector
    -Crea un "Vector" del tipo de dato que prefieras, y añádele 5 elementos. Elimina el 2o y 3er elemento y muestra el resultado final.
    -Indica cuál es el problema de utilizar un Vector con la capacidad por defecto si tuviésemos 1000 elementos para ser añadidos al mismo.

---
+++ Temas7.8y9/src/EjercicioVector.java
@@ -0,0 +1,39 @@
+import java.util.Vector;
+
+public class EjercicioVector {
+
+    public static void main(String[] args) {
+        primerVector();
+        segundoVector();
+    }
+
+    //TODO Crea un vector del tipo de dato que prefieras, y añádele 5 elementos,
+    // Elimina el 2o y 3o elemento y muestra el resultado final
+    public static void primerVector() {
+        System.out.println("---Mostrar solo 3 elementos----");
+        Vector<String> vector = new Vector<>();
+        vector.add("Real Madrid");
+        vector.add("Real Sociedad");
+        vector.add("Barsa");
+        vector.add("Bayern de Munich");
+        vector.add("Club Atletico de Madrid");
+        vector.remove(2);
+        vector.remove(3);
+        System.out.println("Hemos eliminado dos elementos con remove() y ahora son "+vector.size()+" elementos:");
+        for (String equipo : vector) {
+            System.out.println(equipo);
+        }
+    }
+
+    //FIXME indica cuál es el problema de utilizar un vector
+    // con la capacidad por defecto si tuviésemos 1000 elementos
+    // para ser añadidos al mismo
+    public static void segundoVector() {
+        System.out.println("----------");
+        System.out.println("Indica cual es el problema de utilizar un vector" +
+                "con la capacidad por defecto su tuviesemos 1000 elementos " +
+                "para ser añadidos al mismo");
+        System.out.println("Respuesta--->  Sobre mucha memoria y cada vez se duplica más la dimensión del vector");
+    }
+
+}
