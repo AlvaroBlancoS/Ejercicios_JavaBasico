9a2ed2306bc31b1f640a8ccd24bbe648b815ac1b
AlvaroBlancoS <alvaroblancon.sangines.to@gmail.com>
2022-10-03 10:45:42 +0200

    Ejercicios de tema 7, 8 y 9

---
+++ Temas7.8y9/src/EjercicioReverse.java
@@ -0,0 +1,16 @@
+//TODO Escribe el código que devuelva una cadena al revés.
+// Por ejemplo, la cadena "hola mundo", debe retornar "odnum aloh".
+public class EjercicioReverse {
+    public static void main(String[] args) {
+        String texto ="Hola mundo";
+        System.out.println(reverse(texto.toLowerCase()));
+    }
+    public static String reverse(String texto) {
+
+        return new StringBuilder(texto).reverse().toString();
+    }
+}
+
+
+
+
